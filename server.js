46764a774b69da985f674b75bc14086a0ee3c3d8
Ricardo Jr. Suyón A. <ricardosuyal@gmail.com>
2024-09-27 09:51:32 -0500

    Update server.js
    para que se despliegue en nube

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+
+// Utiliza la variable de entorno PORT proporcionada por la plataforma o usa el puerto 3000 por defecto
+const port = process.env.PORT || 3000;
 
 // Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,6 +12,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, '0.0.0.0',() => {
+// Escuchar en el puerto asignado por el entorno
+app.listen(port, '0.0.0.0', () => {
     console.log(`Servidor escuchando en http://0.0.0.0:${port}`);
 });
